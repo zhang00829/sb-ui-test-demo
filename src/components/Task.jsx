2382836a48389577a0b3f0bba24fc4409d543fd1
张磊 <zhangl1438@sany.com.cn>
2023-07-14 11:31:08 +0800

    first-change

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,59 +1,60 @@
-import PropTypes from "prop-types";
+
+import PropTypes from 'prop-types';
 export default function Task({
-  task: { id, title, state },
-  onArchiveTask,
-  onTogglePinTask,
-  onEditTitle,
-}) {
+                               task: { id, title, state },
+                               onArchiveTask,
+                               onTogglePinTask,
+                               onEditTitle,
+                             }) {
   return (
-    <div
-      className={`list-item ${state}`}
-      role="listitem"
-      aria-label={`task-${id}`}
-    >
-      <label
-        htmlFor="checked"
-        aria-label={`archiveTask-${id}`}
-        className="checkbox"
+      <div
+          className={`list-item ${state}`}
+          role="listitem"
+          aria-label={`task-${id}`}
       >
-        <input
-          type="checkbox"
-          disabled={true}
-          name="checked"
-          id={`archiveTask-${id}`}
-          checked={state === "TASK_ARCHIVED"}
-        />
-        <span
-          className="checkbox-custom"
-          onClick={() => onArchiveTask("ARCHIVE_TASK", id)}
-          role="button"
-          aria-label={`archiveButton-${id}`}
-        />
-      </label>
-
-      <label htmlFor="title" aria-label={title} className="title">
-        <input
-          type="text"
-          value={title}
-          name="title"
-          placeholder="Input title"
-          style={{ textOverflow: "ellipsis" }}
-          onChange={(e) => onEditTitle(e.target.value, id)}
-        />
-      </label>
-
-      {state !== "TASK_ARCHIVED" && (
-        <button
-          className="pin-button"
-          onClick={() => onTogglePinTask(state, id)}
-          id={`pinTask-${id}`}
-          aria-label={state === "TASK_PINNED" ? "unpin" : "pin"}
-          key={`pinTask-${id}`}
+        <label
+            htmlFor="checked"
+            aria-label={`archiveTask-${id}`}
+            className="checkbox"
         >
-          <span className={`icon-bell`} />
-        </button>
-      )}
-    </div>
+          <input
+              type="checkbox"
+              disabled={true}
+              name="checked"
+              id={`archiveTask-${id}`}
+              checked={state === "TASK_ARCHIVED"}
+          />
+          <span
+              className="checkbox-custom"
+              onClick={() => onArchiveTask("ARCHIVE_TASK", id)}
+              role="button"
+              aria-label={`archiveButton-${id}`}
+          />
+        </label>
+
+        <label htmlFor="title" aria-label={title} className="title">
+          <input
+              type="text"
+              value={title}
+              name="title"
+              placeholder="Input title"
+              style={{ textOverflow: "ellipsis" }}
+              onChange={(e) => onEditTitle(e.target.value, id)}
+          />
+        </label>
+
+        {state !== "TASK_ARCHIVED" && (
+            <button
+                className="pin-button"
+                onClick={() => onTogglePinTask(state, id)}
+                id={`pinTask-${id}`}
+                aria-label={state === "TASK_PINNED" ? "unpin" : "pin"}
+                key={`pinTask-${id}`}
+            >
+              <span className={`icon-star`} />
+            </button>
+        )}
+      </div>
   );
 }
 
